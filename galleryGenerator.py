809f9414e651570910c8fd4d94d50be07d18b759
Sean Rider <sean.rider.01@gmail.com>
2020-04-13 20:44:08 -0500

    More Galleries
    Added more galleries

--- galleryGenerator.py
+++ galleryGenerator.py
@@ -1,8 +1,8 @@
 import os
 
-url = 'E:\Libraries\Documents\GitHub\SeanRiderPhoto.com\Galleries\Flooz2Img\ '[:-1]
+url = 'E:\Libraries\Documents\GitHub\SeanRiderPhoto.com\Galleries\SCImg\ '[:-1]
 
 entries = os.listdir(url)
 
 for entry in entries:
-    print('<div class = "cell><img id ="'+entry+'" src ="'+url+entry+'" alt ="'+entry+'" class="responsive></div>')
+    print('<div class = "gcell"><img id ="'+entry+'" src ="'+url+entry+'" alt ="'+entry+'" class="responsive"></div>')
